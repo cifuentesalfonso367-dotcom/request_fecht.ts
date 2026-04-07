4557401ca22ee513d960017e21752076faf61486
SAOT31 <saot-31@hotmail.com>
2026-04-07 13:22:56 -0500

    Add functionality to open episode and character URLs in browser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ApiService } from "./services/apiServices.js";
 import { Character } from "./models/character.js";
+import open from 'open';
 
 const characterService = new ApiService<Character>("https://rickandmortyapi.com/api/character");
 
@@ -12,6 +13,16 @@ async function ejecutarApp() {
     if (key === 'origin' || key === 'location') return '[Object]';
     return value;
   }, 2));
+
+  if (personaje.data?.episode) {
+    console.log(`Abriendo el enlace del episodio: ${personaje.data.episode}`);
+    await open(personaje.data.episode);
+  }
+  if (personaje.data?.url) {
+    console.log(`Abriendo el enlace del personaje: ${personaje.data.url}`);
+    await open(personaje.data.url);
+  }
+
   console.log("Lista de personajes:");
   console.log(JSON.stringify(listaPersonajes.data, (key, value) => {
     if (key === 'origin' || key === 'location') return '[Object]';
